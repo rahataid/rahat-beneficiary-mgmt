b303b3ad294e9ffef24477a0df2991c816a339c5
Amrit Pandey <pandeyamrit714@gmail.com>
2024-01-24 00:56:39 +0545

    installed tailwind

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,11 +1,7 @@
-import styles from './page.module.css';
-
 export default async function Index() {
   return (
-    <div className={styles.page}>
-      <div className="wrapper">
-        <h3>Hello Next JS!</h3>
-      </div>
+    <div className="text-red-500">
+      <h3>Hello Next JS!</h3>
     </div>
   );
 }
