9e27eab7639ac2a4fa3b3add8d8ab01723f39b44
Amrit Pandey <pandeyamrit714@gmail.com>
2024-01-25 21:17:57 +0545

    dashboard view

---
+++ apps/frontend/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import DashboardView from '@/sections/dashboard/dashboard-view';
+
+export const metadata = {
+  title: 'Dashboard',
+};
+
+export default function Dashboard() {
+  return <DashboardView />;
+}
--- apps/frontend/routes/paths.ts
+++ apps/frontend/routes/paths.ts
@@ -1,9 +1,13 @@
 const ROOTS = {
     AUTH: '/auth',
+    DASHBOARD: '/dashboard'
 }
 
 export const paths = {
     auth: {
         login: `${ROOTS.AUTH}/login`,
+    },
+    dashboard: {
+        root: ROOTS.DASHBOARD,
     }
 }
