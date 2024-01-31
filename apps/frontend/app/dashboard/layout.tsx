feab50f95ad2688a60b829f615ef373e3d78081f
Amrit Pandey <pandeyamrit714@gmail.com>
2024-01-31 17:37:25 +0545

    dashboard common layout fix

--- apps/frontend/app/dashboard/layout.tsx
+++ apps/frontend/app/dashboard/layout.tsx
@@ -34,144 +34,141 @@ export default function DashboardLayout({
   };
 
   return (
-    <main className="px-5 lg:px-10 xl:px-20 h-screen">
-      <section className="h-full">
-        <div className="grid grid-cols-4 h-full">
-          <div className="border-x-2 w-full h-full hidden lg:block">
-            <div className="p-5 h-full">
-              <Image
-                className="mb-20"
-                src="/assets/logo/rahat-logo.png"
-                alt="rahat-logo.png"
-                width={60}
-                height={60}
-              />
-              <div className="flex flex-col justify-between h-[82vh]">
-                <div className="flex flex-col gap-2">
-                  {navData.map((item: NavData) => (
-                    <div
-                      key={item.title}
-                      className={`flex gap-2 p-3  rounded hover:bg-[#DDF6FC] hover:text-[#1F61AC] cursor-pointer ${
-                        currentRoute === item.path &&
-                        'bg-[#DDF6FC] text-[#1F61AC] '
-                      }`}
-                      onClick={() => router.push(item.path)}
-                    >
-                      <Image
-                        key={item.title}
-                        src={item.icon}
-                        alt={item.title.toLowerCase()}
-                        width={30}
-                        height={30}
-                      />
-                      <div className="flex items-center">
-                        <span className="font-bold">{item.title}</span>
-                      </div>
-                    </div>
-                  ))}
+    <div className="flex h-screen px-10">
+      <nav className=" hidden lg:block fixed border-x-2 w-64 p-4 h-full overflow-y-auto">
+        <div>
+          <Image
+            className="mb-20"
+            src="/assets/logo/rahat-logo.png"
+            alt="rahat-logo.png"
+            width={60}
+            height={60}
+          />
+          <div className="flex flex-col justify-between h-[82vh]">
+            <div className="flex flex-col gap-2">
+              {navData.map((item: NavData) => (
+                <div
+                  key={item.title}
+                  className={`flex gap-2 p-3  rounded hover:bg-[#DDF6FC] hover:text-[#1F61AC] cursor-pointer ${
+                    currentRoute === item.path && 'bg-[#DDF6FC] text-[#1F61AC] '
+                  }`}
+                  onClick={() => router.push(item.path)}
+                >
+                  <Image
+                    key={item.title}
+                    src={item.icon}
+                    alt={item.title.toLowerCase()}
+                    width={30}
+                    height={30}
+                  />
+                  <div className="flex items-center">
+                    <span className="font-bold">{item.title}</span>
+                  </div>
                 </div>
-                <div>
-                  <span className="font-bold cursor-pointer" onClick={onGoBack}>
-                    Go Back
-                  </span>
-                </div>
-              </div>
+              ))}
             </div>
-          </div>
-
-          <div className="col-span-4 lg:col-span-3">
-            <div className="p-5 flex justify-between lg:justify-end">
-              <div className="block lg:hidden">
-                <Sheet>
-                  <SheetTrigger>
-                    <Image
-                      src="/assets/svg/burger-menu-icon.svg"
-                      alt="burger-menu-icon"
-                      height={20}
-                      width={30}
-                    />
-                  </SheetTrigger>
-                  <SheetContent side="left">
-                    <div className="p-5 h-full">
-                      <Image
-                        className="mb-10"
-                        src="/assets/logo/rahat-logo.png"
-                        alt="rahat-logo.png"
-                        width={60}
-                        height={60}
-                      />
-                      <div className="flex flex-col justify-between h-full">
-                        <div className="flex flex-col gap-2">
-                          {navData.map((item: NavData) => (
-                            <div
-                              key={item.title}
-                              className={`flex gap-2 p-3  rounded hover:bg-[#DDF6FC] hover:text-[#1F61AC] cursor-pointer ${
-                                currentRoute === item.path &&
-                                'bg-[#DDF6FC] text-[#1F61AC] '
-                              }`}
-                              onClick={() => router.push(item.path)}
-                            >
-                              <Image
-                                key={item.title}
-                                src={item.icon}
-                                alt={item.title.toLowerCase()}
-                                width={30}
-                                height={30}
-                              />
-                              <div className="flex items-center">
-                                <span className="font-bold">{item.title}</span>
-                              </div>
-                            </div>
-                          ))}
-                        </div>
-                        <div>
-                          <span
-                            className="font-bold cursor-pointer"
-                            onClick={onGoBack}
-                          >
-                            Go Back
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </SheetContent>
-                </Sheet>
-              </div>
-
-              <div>
-                <DropdownMenu>
-                  <DropdownMenuTrigger>
-                    <Avatar>
-                      <AvatarImage
-                        src="/assets/svg/profile-icon.svg"
-                        alt="profile-icon"
-                      />
-                    </Avatar>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuGroup className="p-2">
-                      <div className="p-2 flex flex-col">
-                        <span className="font-bold">Rocket</span>
-                        <span>rocket@gmail.com</span>
-                      </div>
-                      <div className="p-2">
-                        <Link href={paths.dashboard.profile}>Profile</Link>
-                      </div>
-                      <div className="p-2">
-                        <Link href={paths.dashboard.root}>Home</Link>
-                      </div>
-                      <div className="text-red-500 p-2">
-                        <Link href={paths.auth.login}>Logout</Link>
-                      </div>
-                    </DropdownMenuGroup>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </div>
+            <div>
+              <span className="font-bold cursor-pointer" onClick={onGoBack}>
+                Go Back
+              </span>
             </div>
-            <div>{children}</div>
           </div>
         </div>
-      </section>
-    </main>
+      </nav>
+      <div className="flex-1 lg:ml-72">
+        <div className="p-4 mb-4 w-full z-10">
+          <div className="flex justify-between lg:justify-end">
+            <div className="block lg:hidden">
+              <Sheet>
+                <SheetTrigger>
+                  <Image
+                    src="/assets/svg/burger-menu-icon.svg"
+                    alt="burger-menu-icon"
+                    height={20}
+                    width={30}
+                  />
+                </SheetTrigger>
+                <SheetContent side="left">
+                  <div className="p-5 h-full">
+                    <Image
+                      className="mb-10"
+                      src="/assets/logo/rahat-logo.png"
+                      alt="rahat-logo.png"
+                      width={60}
+                      height={60}
+                    />
+                    <div className="flex flex-col justify-between h-full">
+                      <div className="flex flex-col gap-2">
+                        {navData.map((item: NavData) => (
+                          <div
+                            key={item.title}
+                            className={`flex gap-2 p-3  rounded hover:bg-[#DDF6FC] hover:text-[#1F61AC] cursor-pointer ${
+                              currentRoute === item.path &&
+                              'bg-[#DDF6FC] text-[#1F61AC] '
+                            }`}
+                            onClick={() => router.push(item.path)}
+                          >
+                            <Image
+                              key={item.title}
+                              src={item.icon}
+                              alt={item.title.toLowerCase()}
+                              width={30}
+                              height={30}
+                            />
+                            <div className="flex items-center">
+                              <span className="font-bold">{item.title}</span>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                      <div>
+                        <span
+                          className="font-bold cursor-pointer"
+                          onClick={onGoBack}
+                        >
+                          Go Back
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </SheetContent>
+              </Sheet>
+            </div>
+
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar>
+                    <AvatarImage
+                      src="/assets/svg/profile-icon.svg"
+                      alt="profile-icon"
+                    />
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="mr-5" side="bottom">
+                  <DropdownMenuGroup className="p-2">
+                    <div className="p-2 flex flex-col">
+                      <span className="font-bold">Rocket</span>
+                      <span>rocket@gmail.com</span>
+                    </div>
+                    <div className="p-2">
+                      <Link href={paths.dashboard.profile}>Profile</Link>
+                    </div>
+                    <div className="p-2">
+                      <Link href={paths.dashboard.root}>Home</Link>
+                    </div>
+                    <div className="text-red-500 p-2">
+                      <Link href={paths.auth.login}>Logout</Link>
+                    </div>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </div>
+
+        <div className="overflow-y-auto">{children}</div>
+      </div>
+    </div>
   );
 }
