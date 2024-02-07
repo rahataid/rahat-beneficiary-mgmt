a0285530f4d77593d2494b215540948d1365bb5a
Binod Chaudhary <thebinod7@gmail.com>
2024-02-07 16:49:43 +0545

    update cron job time

--- apps/rahat-community/src/app/schedulers/schedule.provider.ts
+++ apps/rahat-community/src/app/schedulers/schedule.provider.ts
@@ -6,7 +6,7 @@ import { EVENTS } from '../../constants';
 @Injectable()
 export class ScheduleService {
   constructor(private eventEmitter: EventEmitter2) {}
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_DAY_AT_10PM)
   cleanupTargetQuery() {
     this.eventEmitter.emit(EVENTS.CLEANUP_TARGET_QUERY, {});
   }
