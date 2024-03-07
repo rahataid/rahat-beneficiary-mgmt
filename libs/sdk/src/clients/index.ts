fca61ca0677b2a833e16fd39dfd775b83f63a6c7
RS21 <rs21@pop-os.localdomain>
2024-03-07 17:48:56 +0545

    update: export sdk clients

--- libs/sdk/src/clients/index.ts
+++ libs/sdk/src/clients/index.ts
@@ -1,1 +1,7 @@
 export * from './beneficiary.client';
+export * from './beneficiarySources.client';
+export * from './beneficiarygroup.client';
+export * from './fieldDefinition.client';
+export * from './groups.client';
+export * from './source.client';
+export * from './target.client';
