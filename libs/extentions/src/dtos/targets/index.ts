7dad0fc648e9f4858319388c29d5466d0489e455
karun-rumsan <karun@rumsan.net>
2024-05-09 21:40:28 +0545

    list target dto  added

--- libs/extentions/src/dtos/targets/index.ts
+++ libs/extentions/src/dtos/targets/index.ts
@@ -1,3 +1,4 @@
 export * from './create-target-query.dto';
 export * from './create-target-result-dto';
 export * from './update-target.dto';
+export * from './list-target-query.dto';
---
+++ libs/extentions/src/dtos/targets/list-target-query.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class ListTargetQueryDto {
+  @ApiProperty({ example: 1 })
+  @IsString()
+  @IsOptional()
+  sort!: string;
+
+  @ApiProperty({ example: 'desc' })
+  @IsString()
+  @IsOptional()
+  order!: 'asc' | 'desc';
+
+  @ApiProperty({ example: 1 })
+  @IsNumber()
+  page!: number;
+
+  @ApiProperty({ example: '10' })
+  @IsNumber()
+  perPage!: number;
+
+  @ApiPropertyOptional({ example: 'My Beneficiary' })
+  @IsString()
+  @IsOptional()
+  label?: string;
+}
