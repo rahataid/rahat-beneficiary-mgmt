386c2dd8a508eb7e3b5cbd65a25ab9ce7e21889e
Binod Rumsan <binod.chaudhary@rumsan.net>
2024-08-08 12:51:22 +0545

    generate app identity and save into settings

---
+++ prisma/settings.feed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient, SettingDataType } from '@prisma/client';
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
+
+import { SETTINGS } from './seed-data';
+
+const privateKey = generatePrivateKey();
+const identity = privateKeyToAccount(privateKey);
+
+const settingsData = [
+  {
+    name: SETTINGS.APP_DENTITY,
+    dataType: SettingDataType.OBJECT,
+    requiredFields: ['PRIVATE_KEY', 'PUBLIC_KEY', 'ADDRESS'],
+    value: {
+      PRIVATE_KEY: privateKey,
+      PUBLIC_KEY: identity.publicKey,
+      ADDRESS: identity.address,
+    },
+  },
+];
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (let setting of settingsData) {
+    await prisma.setting.upsert({
+      where: { name: setting.name },
+      update: setting,
+      create: setting,
+    });
+  }
+  console.log('====Settings feed completed====');
+}
+
+main();
